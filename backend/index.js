ba1150145efda72ac64e38d0ff0a5640e42c2776
realProject <tolqinmirsaliyev@gmail.com>
2024-02-23 10:59:17 +0500

    луееш

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,8 @@
-const express= require('express')
+const express = require('express')
+const app  = express()
+require('dotenv').config()
+
+
+app.listen(8800, () =>{
+    console.log(`server to connected`)
+})
