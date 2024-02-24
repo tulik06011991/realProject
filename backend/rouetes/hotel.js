af3183abe7ab419fecbdd82830992d0af4f75ae3
realProject <tolqinmirsaliyev@gmail.com>
2024-02-24 12:35:34 +0500

    dddd

--- backend/rouetes/hotel.js
+++ backend/rouetes/hotel.js
@@ -20,9 +20,7 @@ router.post("/" , async (req, res) =>{
 
 router.put("/:id" , async(req, res) =>{
     try {
-        const {id} = req.params
-
-        const updateHotel = await HotelData.findByIdAndUpdate(id, req.body)
+        const updateHotel = await HotelData.findByIdAndUpdate(req.params.id, req.body)
         res.status(200).json(updateHotel)
         
     } catch (error) {
