f20e0186e327448798e026f655f0ae26dd3b7125
realProject <tolqinmirsaliyev@gmail.com>
2024-02-24 12:27:37 +0500

    ggg

--- backend/rouetes/hotel.js
+++ backend/rouetes/hotel.js
@@ -8,7 +8,7 @@ router.post("/" , async (req, res) =>{
     
     const hotelData = new HotelData(req.body)
     try {
-        const SaveHotel = await hotelData.save()
+        const SaveHotel = await HotelData.create(hotelData)
         res.status(201).json(SaveHotel)
         
     } catch (error) {
@@ -18,8 +18,12 @@ router.post("/" , async (req, res) =>{
     }
 })
 
-router.put("/" , async(req, res) =>{
+router.put("/:id" , async(req, res) =>{
     try {
+        const {id} = req.params
+
+        const updateHotel = await HotelData.findByIdAndUpdate(id, req.body)
+        res.status(200).json(updateHotel)
         
     } catch (error) {
         console.log(error),
@@ -27,23 +31,43 @@ router.put("/" , async(req, res) =>{
     }
 })
 
-router.delete("/" , (req, res) =>{
+router.get("/" , async(req, res) =>{
     try {
+        const allHotel = await HotelData.find()
+        res.status(200).json(allHotel)
+
         
-    } catch (error) { console.log(error),
+    } catch (error) {
+        console.log(error),
         res.status(500).json({msg: error})
-        
     }
 })
 
 
-router.put("/" , (req, res) =>{
-    res.send(`salom get`)
+
+
+
+router.get("/:id" , async(req, res) =>{
+    try {
+        const getHotel = await HotelData.findById(req.params.id)
+        res.status(200).json(getHotel)
+    } catch (error) {
+        console.log(error),
+        res.status(500).json({msg: error})
+    }
 })
 
 
-router.delete("/" , (req, res) =>{
-    res.send(`salom get`)
+router.delete("/:id" , async(req, res) =>{
+    try {
+        const deleteHotel = await HotelData.findByIdAndDelete(req.params.id)
+        res.status(200).json(` o'chirildi`)
+        
+    } catch (error) {
+        console.log(error),
+        res.status(500).json({msg: error})
+        
+    }
 })
 
 
