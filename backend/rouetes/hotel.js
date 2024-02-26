62f4bace38837898f6504ffab4f80bc0c2d9ea92
realProject <tolqinmirsaliyev@gmail.com>
2024-02-26 12:52:06 +0500

    cxvvcxvcx

--- backend/rouetes/hotel.js
+++ backend/rouetes/hotel.js
@@ -1,72 +1,28 @@
 const express= require('express')
 const router = express.Router()
-const HotelData = require("../Model/Hotel")
+
+const {createHotel,
+    updateHotel,
+    deleteHotel,
+    getAllHotel,
+    getIdHotel
+} = require('../Controllers/Hotel')
 
 
-router.post("/" , async (req, res) =>{
-    
-    
-    const hotelData = new HotelData(req.body)
-    try {
-        const SaveHotel = await HotelData.create(hotelData)
-        res.status(201).json(SaveHotel)
-        
-    } catch (error) {
-        console.log(error),
-        res.status(500).json({msg: error})
-        
-    }
-})
+router.post("/" , createHotel )
 
-router.put("/:id", async(req, res) =>{
-    try {
-        const updateHotel = await HotelData.findByIdAndUpdate(req.params.id, req.body)
-        res.status(200).json(updateHotel)
-        
-    } catch (error) {
-        console.log(error),
-        res.status(500).json({msg: error})
-    }
-})
+router.put("/:id", updateHotel )
 
-router.get("/" , async(req, res) =>{
-    try {
-        const allHotel = await HotelData.find()
-        res.status(200).json(allHotel)
-
-        
-    } catch (error) {
-        console.log(error),
-        res.status(500).json({msg: error})
-    }
-})
+router.get("/" , getAllHotel)
 
 
 
 
 
-router.get("/:id" , async(req, res) =>{
-    try {
-        const getHotel = await HotelData.findById(req.params.id)
-        res.status(200).json(getHotel)
-    } catch (error) {
-        console.log(error),
-        res.status(500).json({msg: error})
-    }
-})
+router.get("/:id" ,  getIdHotel)
 
 
-router.delete("/:id" , async(req, res) =>{
-    try {
-        const deleteHotel = await HotelData.findByIdAndDelete(req.params.id)
-        res.status(200).json(` o'chirildi`)
-        
-    } catch (error) {
-        console.log(error),
-        res.status(500).json({msg: error})
-        
-    }
-})
+router.delete("/:id" ,  deleteHotel)
 
 
 
