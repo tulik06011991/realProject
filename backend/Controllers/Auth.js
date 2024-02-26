dd97b5bcfaa8d1ea429eb0aa1ec55da47e23c60f
tulik <tolqinmirsaliyev@gmail.com>
2024-02-26 22:50:06 +0300

    vvcvcvc

--- backend/Controllers/Auth.js
+++ backend/Controllers/Auth.js
@@ -1,1 +1,37 @@
-const auth = require('../Model/Auth')
+const AuthModel = require('../Model/Auth');
+
+// Foydalanuvchi ro'yxatdan o'tkazish
+const registerUser = async (req, res) => {
+    try {
+        // Foydalanuvchi ma'lumotlarini olamiz
+        const { username, email, password } = req.body;
+
+        // Foydalanuvchi ro'yxatdan o'tkaziladi
+        const newUser = await AuthModel.create({ username, email, password });
+
+        res.status(201).json(newUser);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+// Foydalanuvchi kiritish
+const loginUser = async (req, res) => {
+    try {
+        // Foydalanuvchi ma'lumotlarini olamiz
+        const { username, password } = req.body;
+
+        // Foydalanuvchi tekshiriladi
+        const user = await AuthModel.findOne({ username });
+
+        if (!user || user.password !== password) {
+            return res.status(401).json({ message: `'Noto'g'ri foydalanuvchi nomi yoki parol'!'` });
+        }
+
+        res.status(200).json({ message: 'Foydalanuvchi kirish muvaffaqiyatli!' });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+module.exports = { registerUser, loginUser };
