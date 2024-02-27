d469b41f648b9f9a35c85ac246d4182b290ee3d9
realProject <tolqinmirsaliyev@gmail.com>
2024-02-27 10:59:10 +0500

    xxxx

--- backend/rouetes/auth.js
+++ backend/rouetes/auth.js
@@ -3,13 +3,8 @@ const router = express.Router()
 const register = require('../Controllers/Auth')
 
 
-<<<<<<< HEAD
+
 router.post("/" , register )
-=======
-router.post("/", (req, res) =>{
-    res.send(`salom get`)
-})
->>>>>>> 22e7df7a7808cc9f088f1dd17b1d7ee39092fa51
 
 router.post("/" , login )
 
