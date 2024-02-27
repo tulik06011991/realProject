a2de44970c40a4b06e687e615fcb939eb9a2afcf
realProject <tolqinmirsaliyev@gmail.com>
2024-02-27 10:17:04 +0500

    vcvcvxc

--- backend/Model/Users.js
+++ backend/Model/Users.js
@@ -1,1 +1,57 @@
 const mongoose  = require('mongoose')
+
+
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String, 
+        required: true
+
+    },
+    type: {
+        type: String, 
+        required: true
+
+    },
+   
+    city: {
+        type: String, 
+        required: true
+
+    },
+    address:{
+        type: String, 
+        required: true
+        
+    },
+    distance: {
+        type: String, 
+        required: true
+
+    },
+   
+    title: {
+        type: String, 
+        required: true
+
+    },
+    desc: {
+        type: String, 
+        required: true
+
+    },
+   
+    cheapertPrice:{
+        type: Number,
+        required: true
+    },
+    featured: {
+        type: Boolean, 
+        default : false
+
+    },
+   
+}) 
+
+ const hotelModel =  mongoose.model('Hotel', userSchema)
+ module.exports = hotelModel
