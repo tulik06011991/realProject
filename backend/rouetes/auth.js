75f6e06b562c22e968b48df8a372e754311f191f
realProject <tolqinmirsaliyev@gmail.com>
2024-02-27 09:55:40 +0500

    nhgg

--- backend/rouetes/auth.js
+++ backend/rouetes/auth.js
@@ -1,32 +1,12 @@
 const express= require('express')
 const router = express.Router()
+const register = require('../Controllers/Auth')
 
 
-router.post("/" , (req, res) =>{
-    res.send(`salom get`)
-})
+router.post("/" , register )
 
-router.get("/" , (req, res) =>{
-    res.send(`salom get`)
-})
+router.post("/" , login )
 
-router.get("/" , (req, res) =>{
-    res.send(`salom get`)
-})
-
-router.get("/register" , (req, res) =>{
-    res.send(`salom register`)
-})
-
-
-router.put("/" , (req, res) =>{
-    res.send(`salom get`)
-})
-
-
-router.delete("/" , (req, res) =>{
-    res.send(`salom get`)
-})
 
 
 
