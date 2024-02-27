37dd8a01edfee08532df8d3ae74a1b7e7b9cca1c
realProject <tolqinmirsaliyev@gmail.com>
2024-02-27 10:50:21 +0500

    cccc

--- backend/Controllers/Auth.js
+++ backend/Controllers/Auth.js
@@ -2,25 +2,36 @@ const AuthModel = require('../Model/Auth');
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 
-// Foydalanuvchi ro'yxatdan o'tkazish
+const bcrypt = require('bcrypt');
+const AuthModel = require('./AuthModel'); // AuthModelni qo'shing yoki avvaldan yaratgan bo'lsangiz uni chaqiring
+
 const registerUser = async (req, res) => {
     try {
-        const isEmpty = await Object.values(req.body).some((item) =>item ===!item)
-        if(isExsist) {
-            res.status(400).json(`siz hamma bo'sh joylarni to'ldirmadingiz`);
-
+        const isEmpty = await Object.values(req.body).some((item) => item === !item);
+        if (isEmpty) {
+            return res.status(400).json(`siz hamma bo'sh joylarni to'ldirmadingiz`);
         }
-        const isUser = await AuthModel.findOne({username: req.body.username}) 
-        if(isUser) {
-            res.status(401).json(` Bu foydalanuvchi ro'yxatdan o'tgan `)
-
+        
+        const isUser = await AuthModel.findOne({ username: req.body.username });
+        if (isUser) {
+            return res.status(401).json(`Bu foydalanuvchi ro'yxatdan o'tgan`);
         }
-        const hashPassword = await bcrypt.hash(req.body.password, 10)
-        const newUser = await AuthModel.create({...req.body, password: hashPassword})
+
+        const hashPassword = await bcrypt.hash(req.body.password, 10);
+        const newUser = await AuthModel.create({ ...req.body, password: hashPassword });
+
+        const token = jwt.sign({id:newUser._id, username: newUser.username})
+
+        const {}
+        
+        res.status(201).json(newUser); // Foydalanuvchi muvaffaqiyatli qo'shildi
+
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
 };
+
+module.exports = registerUser;
 
 // Foydalanuvchi kiritish
 const loginUser = async (req, res) => {
