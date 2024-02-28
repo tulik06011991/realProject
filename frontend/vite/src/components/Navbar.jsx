dec8a11ddb9768cd0e4a0a333115dda7cc497fcb
realProject <tolqinmirsaliyev@gmail.com>
2024-02-28 12:32:50 +0500

    cc

--- frontend/vite/src/components/Navbar.jsx
+++ frontend/vite/src/components/Navbar.jsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
+    
 
     const toggleMenu = () => {
         setIsMenuOpen(!isMenuOpen);
