031cead7bd0f19f176b4748a461c6c9b205c2bcb
realProject <tolqinmirsaliyev@gmail.com>
2024-02-28 12:20:39 +0500

    urra

--- frontend/vite/src/App.jsx
+++ frontend/vite/src/App.jsx
@@ -5,6 +5,8 @@ import viteLogo from '/vite.svg'
 import { Route, Routes,} from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Login from './components/Login'
+import Register from './components/Register'
+
 
 function App() {
   
@@ -14,6 +16,7 @@ function App() {
     <Navbar/>
     <Routes>
       <Route path='/login' element ={<Login/>}/>
+      <Route path='/register' element ={<Register/>}/>
     </Routes>
 
     
--- frontend/vite/src/components/Navbar.jsx
+++ frontend/vite/src/components/Navbar.jsx
@@ -83,7 +83,7 @@ const Navbar = () => {
                                     <div className="flex space-x-4">
                                         
                                         <Link to="/login" className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Login</Link>
-                                        <Link to="#" className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Register</Link>
+                                        <Link to="/register" className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Register</Link>
                                     </div>
                                 </div>
                         </div>
