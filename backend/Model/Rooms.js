29cf9c0d788c57104264c6e6f3a7d5ea6fdce353
tulik <tolqinmirsaliyev@gmail.com>
2024-02-28 22:34:22 +0300

    ttrtrr

--- backend/Model/Rooms.js
+++ backend/Model/Rooms.js
@@ -1,1 +1,34 @@
 const mongoose  = require('mongoose')
+
+
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String, 
+        required: true,
+        unique: true
+
+    },
+    email: {
+        type: String, 
+        required: true,
+        unique: true
+
+    },
+   
+    password: {
+        type: String, 
+        required: true
+
+    },
+   
+    isAdmin: {
+        type: Boolean, 
+        default : false
+
+    },
+   
+},{timestamps: true}) 
+
+ const hotelModel =  mongoose.model('User', userSchema)
+ module.exports = hotelModel
