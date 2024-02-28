bafbe9437fc0d5e56ac166cb3b02c73556e2d71c
realProject <tolqinmirsaliyev@gmail.com>
2024-02-28 11:50:13 +0500

    sdddd

--- frontend/vite/src/components/Navbar.jsx
+++ frontend/vite/src/components/Navbar.jsx
@@ -33,7 +33,7 @@ const Navbar = () => {
               
               <div className="hidden sm:ml-6 sm:block">
                 <div className="flex space-x-4">
-                  <a href="#" className="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium" aria-current="page">Dashboard</a>
+                  <a href="#"className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Dashboard</a>
                   <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Team</a>
                   <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Projects</a>
                   <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Calendar</a>
