cb740ffb4ae7eb5a746ba7f1151d1e30e0ce182a
realProject <tolqinmirsaliyev@gmail.com>
2024-02-28 11:47:32 +0500

    sdfsdfsdf

--- frontend/vite/src/App.jsx
+++ frontend/vite/src/App.jsx
@@ -1,16 +1,19 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
 
+import {BrowserRouter as Router, Route, Routes, BrowserRouter} from 'react-router-dom'
+import Navbar from './components/Navbar'
 
 function App() {
   
 
   return (
     <>
+    
     <Navbar/>
 
+    
     </>
     
   )
