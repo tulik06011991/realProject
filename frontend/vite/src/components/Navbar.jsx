cfa18d3a042fee9b3733749a3e488389926c31dc
realProject <tolqinmirsaliyev@gmail.com>
2024-03-01 10:11:03 +0500

    sss

--- frontend/vite/src/components/Navbar.jsx
+++ frontend/vite/src/components/Navbar.jsx
@@ -55,20 +55,16 @@ const Navbar = () => {
 
 
                             </div>
+                            <div class="flex space-x-4">
+                                    <a href="/" class="text-gray-300 hover:bg-gray-700 hover:text-white ml-5 rounded-md px-3 py-2 text-sm font-medium">Register</a>
+                                    <a href="/" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Login</a>
+                                </div>
                         </div>
                     </div>
                 </div>
 
-
-                <div class="sm:hidden" id="mobile-menu">
-                    <div class="space-y-1 px-2 pb-3 pt-2">
-
-                        <a href="#" class="bg-gray-900 text-white block rounded-md px-3 py-2 text-base font-medium" aria-current="page">Dashboard</a>
-                        <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Team</a>
-                        <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Projects</a>
-                        <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Calendar</a>
-                    </div>
-                </div>
+                
+                
             </nav>
 
 
