75c4b48a15f5731801c25fc0e8d1cb5bf5ed05f3
realProject <tolqinmirsaliyev@gmail.com>
2024-03-01 10:57:33 +0500

    sss

--- frontend/vite/src/App.jsx
+++ frontend/vite/src/App.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import Navbar from './components/Navbar'
-import {Routes, Route} from  'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
+import Register from './components/Register'
+import Login from './components/Login'
 
 
 const App = () => {
   return (
     <>
-<Navbar/>
+      <Navbar />
+      <Routes>
+        <Route path='/register' element={<Register />} />
+        <Route path='/login' element={<Login />} />
+      </Routes>
 
     </>
   )
