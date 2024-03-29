966065d836ef3b4fb5ace1d3fb1627c4ede0fbaa
realProject <tolqinmirsaliyev@gmail.com>
2024-03-29 11:58:10 +0500

    dffdfd

--- frontend/vite/src/components/Register.jsx
+++ frontend/vite/src/components/Register.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import axios from 'axios';
 
 const Register = () => {
-    const [username, setusername] = useState('')
+  const [username, setusername] = useState('')
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
@@ -23,14 +23,14 @@ const Register = () => {
           },
           // withCredentials: true
         },
-        );
-        if(response.data.status ===201){
-          alert.apply(`muvaffaqiyatli`)
-        }else{
-          alert(`bomadi`)
+      );
+      if (response.data.status === 201) {
+        alert.apply(`muvaffaqiyatli`)
+      } else {
+        alert(`bomadi`)
 
-        }
-        console.log(response)
+      }
+      console.log(response)
       // Handle response as needed
     } catch (error) {
       // Handle error
@@ -43,125 +43,45 @@ const Register = () => {
 
 
 
-      <form   onSubmit={handleSubmit} >
-        <br /><br />
-      <input
-                  id="username"
-                  name="username"
-                  type="text"
-                  autoComplete="username"
-                  required
-                  onChange={(e) => setusername(e.target.value)}
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                /> <br /><br />
-                 <input
-                  id="email"
-                  name="email"
-                  type="email"
-                  autoComplete="email"
-                  required
-                  onChange={(e) => setEmail(e.target.value)}
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                /> <br /> <br />
-                <input
-                  id="password"
-                  name="password"
-                  type="password"
-                  autoComplete="current-password"
-                  required
-                  onChange={(e) => setPassword(e.target.value)}
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                />
-                <br /><br />
-                <button
-                type="submit"
-                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-              >
-                Sign in
-              </button>
+      <form onSubmit={handleSubmit} >
 
 
 
 
-
-
-
-      </form>
-      {/* <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
-        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-          <form className="space-y-6" onSubmit={handleSubmit}>
-
-          <div>
-              <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
-                Username
-              </label>
-              <div className="mt-2">
-                <input
-                  id="username"
-                  name="username"
-                  type="text"
-                  autoComplete="username"
-                  required
-                  onChange={(e) => setusername(e.target.value)}
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                />
+        
+        <div class="h-screen bg-indigo-100 flex justify-center items-center">
+          <div class="lg:w-2/5 md:w-1/2 w-2/3">
+            <form class="bg-white p-10 rounded-lg shadow-lg min-w-full">
+              <h1 class="text-center text-2xl mb-6 text-gray-600 font-bold font-sans">Formregister</h1>
+              <div>
+                <label class="text-gray-800 font-semibold block my-3 text-md" for="username">Username</label>
+                <input class="w-full bg-gray-100 px-4 py-2 rounded-lg focus:outline-none" type="text" name="username" id="username"  autoComplete="username"
+                  required  onChange={(e) => setusername(e.target.value)} placeholder="username" />
               </div>
-            </div>
-            <div>
-              <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
-                Email address
-              </label>
-              <div className="mt-2">
-                <input
-                  id="email"
-                  name="email"
-                  type="email"
-                  autoComplete="email"
+              <div>
+                <label class="text-gray-800 font-semibold block my-3 text-md" for="email">Email</label>
+                <input class="w-full bg-gray-100 px-4 py-2 rounded-lg focus:outline-none" type="text" name="email" id="email" autoComplete="email"
                   required
-                  onChange={(e) => setEmail(e.target.value)}
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                />
+                  onChange={(e) => setEmail(e.target.value)} placeholder="@email" />
               </div>
-            </div>
-
-            <div>
-              <div className="flex items-center justify-between">
-                <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
-                  Password
-                </label>
-                
-              </div>
-              <div className="mt-2">
-                <input
-                  id="password"
-                  name="password"
-                  type="password"
-                  autoComplete="current-password"
+              <div>
+                <label class="text-gray-800 font-semibold block my-3 text-md" for="password">Password</label>
+                <input class="w-full bg-gray-100 px-4 py-2 rounded-lg focus:outline-none" type="text" name="password" id="password" autoComplete="current-password"
                   required
-                  onChange={(e) => setPassword(e.target.value)}
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                />
+                  onChange={(e) => setPassword(e.target.value)} placeholder="password" />
               </div>
-            </div>
-
-            <div>
-              <button
-                type="submit"
-                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-              >
-                Sign in
-              </button>
-            </div>
-          </form>
-
-          <p className="mt-10 text-center text-sm text-gray-500">
-            Not a Link member?
-            <Link to="/" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
-              Start Link 14 day free trial
-            </Link>
-          </p>
+              
+              <br />
+              <button type="submit" className=' transition duration-200 bg-blue-500  hover:bg-blue-600 focus:bg-blue-700 focus:shadow-sm focus:ring-4 focus:ring-blue-500 focus:ring-opacity-50 text-dark w-full py-2.5 rounded-lg text-sm shadow-sm hover:shadow-md font-semibold text-center inline-block' >Register</button>
+              <br />
+            
+            </form>
+            
+          </div>
         </div>
-      </div> */}
+        
+      </form>
+      
     </div>
   );
 };
