7addec04496c709e3e555fef7740802227a0d9f8
realProject <tolqinmirsaliyev@gmail.com>
2024-03-29 12:30:58 +0500

    dsdsd

--- backend/Model/Hotel.js
+++ backend/Model/Hotel.js
@@ -8,28 +8,6 @@ const HotelSchema = new mongoose.Schema({
         required: true
 
     },
-    type: {
-        type: String, 
-        required: true
-
-    },
-   
-    city: {
-        type: String, 
-        required: true
-
-    },
-    address:{
-        type: String, 
-        required: true
-        
-    },
-    distance: {
-        type: String, 
-        required: true
-
-    },
-   
     title: {
         type: String, 
         required: true
@@ -40,28 +18,16 @@ const HotelSchema = new mongoose.Schema({
         required: true
         
     },
-    rating: {
+    price: {
         type: Number, 
         min: 0,
         max: 5
 
     },
-    rooms: {
-        type: [String], 
-       
-
-    },
-   
-    cheapertPrice:{
-        type: Number,
+    image{
+        type: String,
         required: true
-    },
-    featured: {
-        type: Boolean, 
-        default : false
-
-    },
-   
+    }
 }) 
 
  const hotelModel =  mongoose.model('Hotel', HotelSchema)
--- frontend/vite/src/components/Products.jsx
+++ frontend/vite/src/components/Products.jsx
@@ -3,15 +3,12 @@ import axios from 'axios'
 
 
 const Products = () => {
-    const [] = useState('')
-    const [] = useState('')
-    const [] = useState('')
-    const [] = useState('')
-    const [] = useState('')
-    const [] = useState('')
-    const [] = useState('')
-    const [] = useState('')
-    const [] = useState('')
+    const [name, setName] = useState('')
+    const [title, setTitle] = useState('')
+    const [desc, setDesc] = useState('')
+    const [price, setPrice] = useState('')
+    const [image, setImage] = useState('')
+ 
   return (
     <div>
 
