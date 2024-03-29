4483db8a7abf3c90a481033ecec9bffd91b2f77a
realProject <tolqinmirsaliyev@gmail.com>
2024-03-29 12:16:52 +0500

    gfgfg

--- frontend/vite/src/components/Login.jsx
+++ frontend/vite/src/components/Login.jsx
@@ -37,7 +37,7 @@ const Login = () => {
 <form onSubmit={handleSubmitt}>
 <div className="min-h-screen bg-gray-100 flex flex-col justify-center sm:py-12">
   <div className="p-10 xs:p-0 mx-auto md:w-full md:max-w-md">
-    <h1 className="font-bold text-center text-2xl mb-5">Your Logo</h1>  
+    <h1 className="font-bold text-center text-2xl mb-5">Kirish</h1>  
     <div className="bg-white shadow w-full rounded-lg divide-y divide-gray-200">
       <div className="px-5 py-7">
         <label className="font-semibold text-sm text-gray-600 pb-1 block">E-mail</label>
