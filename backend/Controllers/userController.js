e2cc55466c1f605f551a0817cf190ed28c8a6103
tulik <tolqinmirsaliyev@gmail.com>
2024-04-04 22:24:35 +0300

    ishladi

--- backend/Controllers/userController.js
+++ backend/Controllers/userController.js
@@ -3,32 +3,43 @@ const UserData= require("../Model/Users");
 const UserProducts = require("../Model/UserProducts")
 
 
-const UserPurchase = async(req, res) =>{
+const UserPurchase = async (req, res) => {
     try {
-        const {UserId, ProductId} = req.body;
+        const { UserId, ProductIds } = req.body; // ProductIds nomli massiv
 
         const user = await UserData.findOne(UserId);
-        const product = await Product.findOne(ProductId)
 
-        if(!user || !product ){
-            res.status(400).send(`malumot topilmadi`)
-        };
-        const daqiqa = Date.now()
+        if (!user) {
+            res.status(400).send(`Foydalanuvchi topilmadi`);
+            return; // Agar foydalanuvchi topilmagan bo'lsa, funksiyani to'xtatamiz
+        }
+
+        const daqiqa = Date.now();
 
         const email = user.email;
         const name = user.username;
-        const productName = product.name;
-        const productPrice = product.price
 
-        const Data = await UserProducts.create({user: email, product: productName, price: productPrice, purchaseDate: daqiqa});
-        res.status(201).json(Data)
+        // Mahsulotlar ro'yxati
+        const products = await Promise.all(ProductIds.map(async (ProductId) => {
+            const product = await Product.findOne(ProductId);
+            return {
+                name: product.name,
+                price: product.price
+            };
+        }));
+
+        const Data = await Promise.all(products.map(async (product) => {
+            return await UserProducts.create({ user: email, product: product.name, price: product.price, purchaseDate: daqiqa });
+        }));
+
+        res.status(201).json(Data);
 
     } catch (error) {
         console.log(error);
         res.status(500).json({ msg: error });
-        
     }
 };
+
 const UserGetProduct = async(req, res) =>{
     try {
         const {UserId, ProductId} = req.body;
